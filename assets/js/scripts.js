c0c2cb139867f46ec888d63baa0dc87cb10a4680
abed kahlout <messikh45@gmail.com>
2021-12-15 13:55:10 +0200

    edit 1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -54,3 +54,10 @@ $( document ).ready(function() {
 
 
 
+$('#r11').on('click', function(){
+    $(this).parent().find('a').trigger('click')
+  })
+  
+  $('#r12').on('click', function(){
+    $(this).parent().find('a').trigger('click')
+  })
